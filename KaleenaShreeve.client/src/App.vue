2e6cb31911eda4f478135fbdf569f34605a749ef
chesterjgreen <seejarom@gmail.com>
2022-09-07 22:43:14 -0600

    tinkering with landing page

--- KaleenaShreeve.client/src/App.vue
+++ KaleenaShreeve.client/src/App.vue
@@ -5,9 +5,22 @@
   <main>
     <router-view />
   </main>
-  <footer>
-    <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+  <footer class="container-fluid">
+    <div class="row bg-dark">
+      <div class="col-8">
+        <div class="row">
+          <div class="col-12 p-4">
+            Premium Reading Experience
+          </div>
+          <div class="col-12 bg-dark text-light text-center p-4">
+            Site Ingredients: Love, Happiness, Truth, Creativity, Cowboys, Romance, Kissing, Heartstrings, Happily Ever Afters
+          </div>
+
+        </div>
+      </div>
+    <div class="col-4">
+
+    </div>
     </div>
   </footer>
 </template>
